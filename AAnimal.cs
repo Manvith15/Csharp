5fa69a53b0714d6bda7309c0dd192229e94eeade
Manvith15 <poojarymanvith17@gmail.com>
2025-09-04 10:44:40 +0530

    Add files via upload

---
+++ AAnimal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Animal
+{
+    static void speak()
+    {
+        Console.WriteLine("animals cant speak:");
+
+    }
+    class Dog : Animal
+    {
+      static void Bark()
+        {
+            Console.WriteLine("Dog barks");
+        }
+    }
+}
+
+class program
+{
+
+}
