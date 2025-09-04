432c5605e7c451cff09d920496c0fd54f3b81060
Manvith15 <poojarymanvith17@gmail.com>
2025-09-04 10:47:10 +0530

    Add files via upload

---
+++ firstandlast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a num:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int lastDigit = num % 10;
+        int firstDigit = num;
+        while (firstDigit >= 10)
+        {
+            firstDigit /= 10;
+        }
+        Console.WriteLine("First digit:" + firstDigit);
+        Console.WriteLine("Last digit:" + lastDigit);
+    }
+    }
