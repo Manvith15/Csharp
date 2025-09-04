470ff719367916c66e987addbf62a089198259e5
Manvith15 <poojarymanvith17@gmail.com>
2025-09-04 10:48:47 +0530

    Add files via upload

---
+++ prg8.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+using ClassLibrary1;
+namespace N
+{
+
+    class program
+    {
+
+        public void Main()
+        {
+            class1 p=new class1();
+            p.Display();
+        }
+    }
+}
