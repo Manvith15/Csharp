86ec7f530ee75c1a1db3a9496d5c7bfb90daa69c
Manvith15 <poojarymanvith17@gmail.com>
2025-09-04 10:47:40 +0530

    Add files via upload

---
+++ getset.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Person
+{
+    private int age;
+
+    public int Age
+    {
+        get { return age; }
+        set
+        {
+            if (age >= 0 && age <= 100)
+                age = value;
+            else
+                Console.WriteLine("age is invalid");
+        }
+    }
+}
+
+class vaanar
+{
+    static void Main()
+    {
+        Person p = new Person();
+
+        p.Age = 25;
+        Console.WriteLine("Age: " + p.Age);
+
+        p.Age = -5;
+        Console.WriteLine("Age: " + p.Age);
+    }
+}
